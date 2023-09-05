7e1dc988794e2a8e23484a0a208cdf2cfc0918ef
Nakaishi <esmt.n2006@gmail.com>
2023-09-05 11:43:41 +0900

    疲れた

--- MaruBatsu.App/ViewController.swift
+++ MaruBatsu.App/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     // 表示中の問題番号を格納
     var currentQuestionNum: Int = 0
     // 問題
-    let questions: [[String: Any]] = [
+    var questions: [[String: Any]] = [
         //        [
         //            "question": "iPhoneアプリを開発する統合環境はZcodeである",
         //            "answer": false
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         //            "answer": true
         //        ]
     ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,7 +69,7 @@ class ViewController: UIViewController {
         showQuestion()
     }
     
-    // 採点アラートを表示する関数
+    // アラートを表示する関数
     func showAlert(message: String) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
@@ -79,28 +80,16 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    //注意アラートを表示する関数
-    func cautionAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
-        
-        //定数alertに定数closeを追加する
-        alert.addAction(close)
-        
-        //animationがonの状態でalertを表示する、alert実行後は何もしない
-        present(alert, animated: true, completion: nil)
-    }
-    
     @IBAction func tappedNoButton(_ sender: Any) {
         if questions.count == 0 {
-            cautionAlert(message: "問題がないよ") } else {
+            showAlert(message: "問題がないよ") } else {
                 checkAnswer(yourAnswer: false)
             }
         
     }
     @IBAction func tappedYesButton(_ sender: Any) {
         if questions.count == 0 {
-        cautionAlert(message: "問題がないよ") } else {
+        showAlert(message: "問題がないよ") } else {
             checkAnswer(yourAnswer: true)
         }
     }
